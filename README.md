97141628e8acd21f122e830bc083788f4c9005fa
Philip Huppert <philip@zeilen-sprung.de>
2014-09-26 14:42:07 +0200

    Added support for OpenVPN 2.3.X.

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 # OpenVPN credentials extractor
 
-This repository contains a plugin for [Volatility](https://code.google.com/p/volatility/) that can extract credentials from the memory of an [OpenVPN](http://openvpn.net/index.php/open-source/) process. The username and password entered by the user, as well as passwords entered to unlock a private key can be recovered. The `--auth-nocache` flag must not be set. Currently only OpenVPN 2.2.2 on Windows is supported.
+This repository contains a plugin for [Volatility](https://code.google.com/p/volatility/) that can extract credentials
+from the memory of an [OpenVPN](http://openvpn.net/index.php/open-source/) process. The username and password entered
+by the user, as well as passwords entered to unlock a private key can be recovered. OpenVPN's `--auth-nocache` flag
+must not be set. The plugin supports OpenVPN 2.X.X on Windows. It was successfully tested with OpenVPN 2.2.2, 2.3.2 and
+2.3.4 on Windows XP (x86) and Windows 7 (x86 & x64).
 
 This repository also contains a small plugin to extract base64/PEM encoded RSA private keys from memory.
 
+### Motivation
+
+This plugin was developed as a part of a university assignment about virtual machine introspection. The Volatility
+framework was chosen, because it offers a wide variety of plugins and can interface with hypervisors through
+[libvmi](https://code.google.com/p/vmitools/) to perform introspection. OpenVPN was chosen as a target, because it is
+widely deployed at the university to facilitate network access control. This allowed to evaluate the security of the
+OpenVPN deployment and demonstrate the plugin on an application that students are familiar with in everyday use.
+
+In a real-world scenario, the plugin may be handy to extract credentials during an investigation or pentest engagement.
+You can also use it to validate that OpenVPN's `--auth-nocache` flag works as intended.
+
 ### Installation
 
-Either place the plugins into Volatility's `plugins/` directory, or use the `--plugins=` option to tell Volatility where to look for plugins.
+Either place the plugins into Volatility's `plugins/` directory, or use the `--plugins=` option to point Volatility
+to the directory containing `openvpn.py`.
 
 ### Usage
 
-The plugins expect no further arguments, just load a memory image and specify a profile for Volatility:
+The plugins expect no further arguments, just load a memory image and specify a profile for Volatility.
+A memory sample can be downloaded from https://mega.co.nz/#!Wx5kiZZS!77NiMTl8B_imwhl4JSg0lmRm90LZ9wgvFhQYxmmOioo.
+After downloading the memory dump, decompress it and run Volatility to extract the credentials:
 
-    ./vol.py -f memory_dump.elf --profile=WinXPSP3x86 openvpn
+    unxz "OpenVPN-2.3.4 XP 32.elf.xz"
+    volatility -f "OpenVPN-2.3.4 XP 32.elf" --profile=WinXPSP3x86 openvpn
--- openvpn.py
+++ openvpn.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # coding=utf-8
 
 """Volatility plugin to extract OpenVPN credentials cached in memory."""
 
 import struct
+import string
 import volatility.plugins.common as common
 import volatility.obj as obj
 import volatility.utils as utils
@@ -14,13 +15,25 @@ __copyright__ = "Copyright 2014, Philip Huppert"
 __license__ = "MIT"
 
 
-class OpenVPN(common.AbstractWindowsCommand):
-    """Extract OpenVPN credentials (username, password) cached in memory.
+USERNAME_CHARSET = string.ascii_letters + string.digits + "_-.@"
 
-    This extractor currently only supports OpenVPN 2.2.2 on Windows,
-    but it should be easy to adapt to other targets. Credentials are available
-    in memory if the client authenticated with a username & password or entered
-    a password to unlock a private key. The --auth-nocache flag must not be set.
+
+def valid_bool(x):
+    return 0 <= x <= 1
+
+
+def terminate_string(s):
+    s, _, _ = s.partition("\0")
+    return s
+
+
+class OpenVPN(common.AbstractWindowsCommand):
+    """Extract OpenVPN client credentials (username, password) cached in memory.
+
+    This extractor supports OpenVPN 2.X.X on Windows. It was successfully tested with OpenVPN 2.2.2, 2.3.2 and 2.3.4
+    on Windows XP (x86) and Windows 7 (x86 & x64). Credentials are available in memory if the client authenticated with
+    a username & password or entered a password to unlock a private key. Furthermore, OpenVPN's --auth-nocache flag
+    must not be set.
     """
 
     def calculate(self):
@@ -30,8 +43,7 @@ class OpenVPN(common.AbstractWindowsCommand):
 
         # Find all OpenVPN processes
         processes = tasks.pslist(kernel_memory)
-        processes = filter(
-            lambda p: str(p.ImageFileName).lower() == "openvpn.exe", processes)
+        processes = filter(lambda p: str(p.ImageFileName).lower() == "openvpn.exe", processes)
 
         # Search for credentials in each process
         for process in processes:
@@ -40,53 +52,69 @@ class OpenVPN(common.AbstractWindowsCommand):
             # Get some basic process information
             pid = int(process.UniqueProcessId)
             image_base = process.Peb.ImageBaseAddress
-            dos_header = obj.Object(
-                "_IMAGE_DOS_HEADER", offset=image_base, vm=process_memory)
+            dos_header = obj.Object("_IMAGE_DOS_HEADER", offset=image_base, vm=process_memory)
             nt_header = dos_header.get_nt_header()
 
-            # Find the .data section
+            # Find the .data and .bss sections
             sections = nt_header.get_sections(True)
-            sections = filter(lambda s: str(s.Name) == ".data", sections)
-            if len(sections) != 1:
-                # Section may be unavailable
+            sections = filter(lambda s: str(s.Name) in [".data", ".bss"], sections)
+            if len(sections) == 0:
+                # Sections may be unavailable
                 continue
 
-            # Determine dimensions of section
-            data_section = sections[0]
-            data_start = data_section.VirtualAddress + image_base
-            data_end = data_start + data_section.Misc.VirtualSize
+            # Search each section for credentials
+            for section in sections:
+                # Determine dimensions of section
+                sec_start = section.VirtualAddress + image_base
+                sec_end = sec_start + section.Misc.VirtualSize
+                sec_type = str(section.Name)
 
-            # Search static user_pass struct
-            # Assumptions:
-            #  - Struct is aligned on 16-byte boundary
-            #  - Bool fields are 4 bytes long
-            #  - Username and password buffers are 4096 bytes long
-            for creds_start in xrange(data_start, data_end, 16):
-                creds = process_memory.read(creds_start, 9)
-                if not creds:
-                    continue
+                # Search static user_pass struct
+                # Assumptions:
+                # - Struct is aligned on 16-byte boundary
+                #  - Bool fields are 4 bytes long in 2.2.2
+                #  - Bool fields are 2 bytes long in 2.3.2 and 2.3.4
+                #  - Username and password buffers are 4096 bytes long
+                for creds_start in xrange(sec_start, sec_end, 16):
+                    creds = process_memory.read(creds_start, 16)
+                    if not creds:
+                        # Memory may be unavailable
+                        continue
 
-                # Try to unpack and verify the beginning of the struct
-                defined, nocache, username = struct.unpack("IIc", creds)
-                if defined > 1 or nocache > 1 or username == "\0":
-                    continue
+                    struct_layout = None
+                    struct_length = None
 
-                # Completely unpack the struct
-                creds = process_memory.zread(creds_start, 4+4+4096+4096)
-                defined, nocache, username, password = \
-                    struct.unpack("II4096s4096s", creds)
+                    # Detect the 2.2.2 struct
+                    defined, nocache, username = struct.unpack("II8s", creds)
+                    if sec_type == ".data" \
+                            and valid_bool(defined) \
+                            and valid_bool(nocache) \
+                            and username[0] in USERNAME_CHARSET:
+                        struct_layout = "II4096s4096s"
+                        struct_length = 4 + 4 + 4096 + 4096
 
-                # Truncate string padding
-                username, _, _ = username.partition("\0")
-                password = password.rstrip("\0")
+                    # Detect the 2.3.2/2.3.4 struct
+                    defined, nocache, username = struct.unpack("BB14s", creds)
+                    if sec_type == ".bss" \
+                            and valid_bool(defined) \
+                            and valid_bool(nocache) \
+                            and username[0] in USERNAME_CHARSET:
+                        struct_layout = "BB4096s4096s"
+                        struct_length = 1 + 1 + 4096 + 4096
 
-                # CENSOR PASSWORD
-                #password = "*" * len(password)
+                    if struct_layout is not None:
+                        # Read and parse detected structure
+                        creds = process_memory.zread(creds_start, struct_length)
 
-                yield (pid, username, password)
+                        _, _, username, password = struct.unpack(struct_layout, creds)
 
-                # Stop searching in current process
-                break
+                        # Terminate strings at null byte
+                        username = terminate_string(username)
+                        password = terminate_string(password)
+                        yield (pid, username, password)
+
+                        # Stop searching in current section
+                        break
 
     def render_text(self, outfd, data):
         """Display credentials."""
